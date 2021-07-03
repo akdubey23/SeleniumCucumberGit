d713824ecc7c5e21ece0cda7f292b5398b81ee2f
niteshkumartiwari <niteshkumartiwari@niteshkumarsmbp>
2021-07-03 17:42:40 +0800

    Test in Progress

--- com.project/features/MyApplication.feature
+++ com.project/features/MyApplication.feature
@@ -10,6 +10,18 @@ Positive flow
  Then verify that search results should be added
  
  
+ #Scenario: Check All items in Todo list
+ #And select All button
+#Then user should able to see all added items
+ 
+ #Scenario: Check Active items in todos list
+ #And select Active button
+ #Then user should able to see items Documentation and API References in Active list
+ 
+ #Scenario: Check Completed items in todo list
+ #Then select radio button for item Documentation
+ #Then click on Completed button
+ #Then user should able to see items Documentation in Active Completed list
  
     
   
--- com.project/src/test/java/stepDefination/SmokeTest.java
+++ com.project/src/test/java/stepDefination/SmokeTest.java
@@ -1,4 +1,3 @@
-
 package stepDefination;
 
 import java.io.File;
@@ -24,29 +23,46 @@ public class SmokeTest {
 	
 WebDriver driver;
 	
-@Given("^Login into Safari Driver$")
-public void login_into_Safari_Driver() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
-}
+	@Given("^Login into Safari Driver$")
+	public void Login_into_Safari_Driver() throws Throwable {
+		WebDriver   driver = new SafariDriver();
+	    driver.get("http://todomvc.com/examples/vue/");
+	    driver.manage().window().maximize();
+	}
 
-@Given("^Search and add Documention in todo list$")
-public void search_and_add_Documention_in_todo_list() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
-}
+	@And("^Search and add Documention in todo list$")
+	public void go_to_Vue_js_URL() throws Throwable {
+	//Thread.sleep(5000);
+	WebElement  ele1= driver.findElement(By.xpath("/html/body/section/header/input"));
+	ele1.sendKeys("Documentation");
+		driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys(Keys.ENTER);
+	  
+	}
 
-@Given("^Search and add API References in todos list$")
-public void search_and_add_API_References_in_todos_list() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
-}
-
-@Then("^verify that search results should be added$")
-public void verify_that_search_results_should_be_added() throws Throwable {
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException();
-}
+	@And("^Search and add API References in todos list$")
+	public void add_Documentation_in_todos_list() throws Throwable {
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys("API References");
+		driver.findElement(By.xpath("/html/body/section/header/input")).sendKeys(Keys.ENTER);
+	}
 
 	
+	
+	@Then("^verify that search results should be added$")
+	public void verify_that_search_results_should_be_added() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+		int flag=0;
+	    List<WebElement>mylist= driver.findElements(By.xpath("/html/body/section/section/ul"));
+	    for(int i=0;i<mylist.size(); i++)
+	    {
+	    	String firstele=mylist.get(i).getText();
+	    	if(firstele.contains("Documenatation"))
+	    	{
+	    		flag=1;
+	    	}
+	    }
+	    
+	}
+	
 }
+
